e80ff744a5013ca0cba63408350b90e559069994
Daniel Wehner <dawehner@googlemail.com>
2017-04-03 22:00:14 +0100

    Make the build-api-first.sh script useful

--- scripts/development/build-api-first.sh
+++ scripts/development/build-api-first.sh
@@ -2,17 +2,25 @@
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
 
-DEST_DIR="$BASE_DIR/../api_first"
+DEST_DIR="$BASE_DIR/../test_api_first"
 
 if [ $1 ] ; then
   DEST_DIR="$1"
 fi
 
-composer create-project drupal/api-first-project ${DEST_DIR} --stability dev --no-interaction --no-install
+sudo rm -Rf $DEST_DIR
+composer create-project drupal/api_first_project ${DEST_DIR} --stability dev --no-interaction
 
 cd ${DEST_DIR}
-composer config repositories.api_first path ${BASE_DIR}
+# ??
+# composer config repositories.api_first path ${BASE_DIR}
 
-composer require "drupal/api-first:*" "phpunit/phpunit:~4.8" --no-progress
+# ??
+# composer require "drupal/api-first:*" "phpunit/phpunit:~4.8" --no-progress
+cd docroot
+rm profiles/contrib/api-first/config/install/core.extension.yml
+drush si api_first --db-url=mysql://root@localhost/loc_apifirst --account-pass=test -y
 
-echo "<?php use Drupal\Robo\RoboFileBase; class RoboFile extends RoboFileBase {}" > RoboFile.php
+
+# ??
+# echo "<?php use Drupal\Robo\RoboFileBase; class RoboFile extends RoboFileBase {}" > RoboFile.php
