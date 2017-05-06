f75e0c58f53c80d10f022b75683c4fa24128f8d5
Daniel Wehner <dawehner@googlemail.com>
2017-05-06 22:19:52 +0100

    disable routes

---
+++ modules/api_first_devel/src/EventSubscriber/RoutingDisabler.php
@@ -0,0 +1,38 @@
+<?php
+
+use Drupal\Core\Routing\RouteBuildEvent;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class RoutingDisabler implements EventSubscriberInterface {
+
+  /**
+   * A list of disabled routes.
+   */
+  protected static $disabledRoutes = [
+    'user.page',
+  ];
+
+  /**
+   * Hides routes by denying access to them.
+   *
+   * @param \Drupal\Core\Routing\RouteBuildEvent $event
+   */
+  public function onRouteBuildAlter(RouteBuildEvent $event) {
+    $collection = $event->getRouteCollection();
+    array_walk(static::$disabledRoutes, function ($disabledRouteName) use ($collection) {
+      if ($route = $collection->get($disabledRouteName)) {
+        $route->setRequirement('_access', 'FALSE');
+      }
+    });
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[RoutingEvents::ALTER] = ['onRouteBuildAlter'];
+    return $events;
+  }
+
+}
