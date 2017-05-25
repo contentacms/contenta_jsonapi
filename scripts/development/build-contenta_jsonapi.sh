5089a5fba99afb3446f5c19b41b3f07ab1635a95
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-05-25 22:48:24 +0200

    docs(Install): Add user feedback

--- scripts/development/build-contenta_jsonapi.sh
+++ scripts/development/build-contenta_jsonapi.sh
@@ -9,7 +9,9 @@ if [ $1 ] ; then
 fi
 
 echo -e "Drush adds some permissions that need extra powers to delete.\n\n  * You are about to delete $DEST_DIR"
+echo "Clearing the destination directory. It requires your sudo password."
 sudo rm -Rf $DEST_DIR
+echo -e "Downloading Contenta CMS using composer.\n\t* Executing: composer create-project contentacms/contenta-jsonapi-project ${DEST_DIR} --stability dev --no-interaction"
 composer create-project contentacms/contenta-jsonapi-project ${DEST_DIR} --stability dev --no-interaction
 
 cd ${DEST_DIR}
@@ -19,9 +21,12 @@ composer config repositories.contenta_jsonapi path ${BASE_DIR}
 # ??
 # composer require "contentacms/contenta_jsonapi:*" "phpunit/phpunit:~4.8" --no-progress
 cd web
+echo "Installing Contenta CMS for local usage."
 ../bin/drush si contenta_jsonapi --db-url=sqlite://sites/default/files/.ht.sqlite --account-pass=test -y
 
+echo "Initializing local PHP server."
 ../bin/drush rs &
+
 ../bin/drush uli
 
 # ??
