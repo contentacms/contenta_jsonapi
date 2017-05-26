e82a295a5cbc5633e79cc13866807b9defde23a9
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-05-26 13:07:28 +0200

    feat(Install): Added stand-alone installer

---
+++ installer.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+original_wd=$(pwd)
+mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+git clone git@github.com:contentacms/contenta_jsonapi.git $mytmpdir
+cd $mytmpdir
+read -n 1 -p "Do you wish to install Contenta CMS (y/n)? " answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo -e "\nGreat!"
+else
+    echo -e "\nBye!"
+    exit
+fi
+read -e -p "Where do you want to install it? (Ex: /var/www/contentacms) " install_path
+if echo "$install_path" ;then
+	install_path=$(echo $install_path|sed -e "s:~:$HOME:g")
+	composer run-script install-contenta $install_path --timeout=0
+	cd $original_wd
+else
+    echo -e "\nBye!"
+    exit
+fi
