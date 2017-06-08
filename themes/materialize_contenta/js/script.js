3d9f405960f4486538499114c7cb6cfbb6165b1d
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2017-06-08 10:06:38 +0200

    feat(Theme): Add user facing theme

---
+++ themes/materialize_contenta/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function (Drupal) {
+
+  'use strict';
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function (context, settings) {
+
+      // Place your code here.
+
+    }
+  };
+
+  // We pass the parameters of this anonymous function are the global variables
+  // that this script depend on. For example, if the above script requires
+  // jQuery, you should change (Drupal) to (Drupal, jQuery) in the line below
+  // and, in this file's first line of JS, change function (Drupal) to
+  // (Drupal, $)
+})(Drupal);
---
+++ themes/materialize_contenta/templates/README.md
@@ -0,0 +1,3 @@
+Use this folder to override template, theme and \[pre\]process functions.
+
+Please refer to the @link registry Theme Registry @endlink topic for more info.
