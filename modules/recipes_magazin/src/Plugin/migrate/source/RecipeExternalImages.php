caf085f6f5e51f5aef02a92b463a0b40dfc1446e
Daniel Wehner <dawehner@googlemail.com>
2017-06-13 18:27:03 +0100

    (feature) [recipes] Work on having a working migration for external URLs

---
+++ modules/recipes_magazin/src/Plugin/migrate/source/RecipeExternalImages.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\recipes_magazin\Plugin\migrate\source;
+
+use Drupal\Component\Utility\UrlHelper;
+
+/**
+ * Extends the CSV migration to make it possible to import terms.
+ *
+ * @MigrateSource(
+ *   id = "recipe_magazin__external_images"
+ * )
+ */
+class RecipeExternalImages extends CSV {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initializeIterator() {
+    $values = [];
+    foreach (parent::initializeIterator() as $row) {
+      if (isset($row[$this->configuration['column']])) {
+        foreach (explode(',', $row[$this->configuration['column']]) as $single_image_url) {
+          if (UrlHelper::isExternal($single_image_url)) {
+            // Some URLs are duplicated, as recipes are more or less a clone of
+            // another one. Therefore we key by URL here, so migrate doesn't
+            // believe we have duplicate rows.
+            $values[$single_image_url] = [
+              $this->configuration['column'] => $single_image_url,
+            ];
+          }
+        }
+      }
+    }
+    return new \ArrayIterator($values);
+  }
+
+}
