da10754020edb0dbc2257f62f4d3f61079b7354e
Sally Young <sally@justafish.co.uk>
2017-06-26 12:38:09 +0100

    Add back asking the user whether to install

--- installer.sh
+++ installer.sh
@@ -4,6 +4,13 @@ original_wd=$(pwd)
 mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 git clone --depth=1 git@github.com:contentacms/contenta_jsonapi.git $mytmpdir
 cd $mytmpdir
+read -n 1 -p "Do you wish to install Contenta CMS (y/n)? " answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo -e "\nGreat!"
+else
+    echo -e "\nBye!"
+    exit
+fi
 read -e -p "Where do you want to install it? (Ex: /var/www/contentacms) " install_path
 if echo "$install_path" ;then
 	install_path=$(echo $install_path|sed -e "s:~:$HOME:g")
