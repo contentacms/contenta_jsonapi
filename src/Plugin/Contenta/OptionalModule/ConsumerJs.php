c232b1d1c023745a279c196f03523988ba6b1372
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-08-11 10:07:29 +0200

    feat: introduce the Contenta JS module

---
+++ src/Plugin/Contenta/OptionalModule/ConsumerJs.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\contenta_jsonapi\Plugin\Contenta\OptionalModule;
+
+/**
+ * Contenta JS.
+ *
+ * @ContentaOptionalModule(
+ *   id = "contentajs",
+ *   label = @Translation("Contenta JS"),
+ *   description = @Translation("Provides server side configuration options for Contenta JS. If you use Contenta JS you will need this module."),
+ *   type = "module",
+ * )
+ */
+class ConsumerJs extends AbstractOptionalModule {}
