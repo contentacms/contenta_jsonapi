d0fbdd2f2f0ceff4e7968940c7e7ac9d5c8b0ca8
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-08-12 09:13:22 +0200

    feat: enable Contenta JS by default

---
+++ src/Plugin/Contenta/OptionalModule/ContentaJs.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\contenta_jsonapi\Plugin\Contenta\OptionalModule;
+
+/**
+ * Contenta JS.
+ *
+ * @ContentaOptionalModule(
+ *   id = "contentajs",
+ *   label = @Translation("Contenta JS"),
+ *   description = @Translation("Provides server side configuration options for Contenta JS. If you use Contenta JS you will need this module."),
+ *   type = "module",
+ *   standardlyEnabled = true,
+ * )
+ */
+class ContentaJs extends AbstractOptionalModule {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form = parent::buildForm($form, $form_state);
+
+    $form['contentajs']['project_info'] = [
+      '#type' => 'item',
+      '#description' => $this->t("This module provides a JSON-RPC endpoint that
+      makes auto-configuration of Contenta JS possible."),
+    ];
+
+    return $form;
+  }
+
+}
