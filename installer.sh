594e89d4eede7e59c49d7600ca8772355d6dd7cd
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-08-22 09:26:45 +0200

    feat: improve quick installer

--- installer.sh
+++ installer.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 original_wd=$(pwd)
-mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
-git clone --depth=1 https://github.com/contentacms/contenta_jsonapi.git $mytmpdir
-cd $mytmpdir
 read -n 1 -p "Do you wish to install Contenta CMS (y/n)? " answer
 if echo "$answer" | grep -iq "^y" ;then
     echo -e "\nGreat!"
@@ -13,12 +10,27 @@ else
 fi
 read -e -p "Where do you want to install it? (Ex: /var/www/contentacms) " install_path
 if echo "$install_path" ;then
+    # Clean up the user input.
 	install_path=$(echo $install_path|sed -e "s:~:$HOME:g")
-	composer run-script install-contenta $install_path --timeout=0
-	composer run-script start-contenta $install_path --timeout=0
+
+	# Download Contenta CMS using composer create-project.
+    php -r "readfile('https://raw.githubusercontent.com/contentacms/contenta_jsonapi/8.x-1.x/download.sh');" > download-contentacms.sh
+    chmod a+x download-contentacms.sh
+    ./download-contentacms.sh $install_path
+
+    # Install Contenta CMS using sqlite by default.
+    echo "The Quick Install uses SQLite to install Contenta CMS. This is not suited for production sites."
+    # Set the .env data.
+    ACCOUNT_PASS="$(LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 13)"
+    echo -e "SQLITE_PATH=tmp\nSQLITE_DATABASE=site.sqlite\nSITE_MAIL=admin@localhost\nACCOUNT_MAIL=admin@localhost\nSITE_NAME='Contenta CMS Demo'\nACCOUNT_NAME=admin\nACCOUNT_PASS=$ACCOUNT_PASS\n" > $install_path/.env
+    echo -e "ACCOUNT_PASS=$ACCOUNT_PASS\n" >> $install_path/.env.local
+    # Install using SQLite.
+    composer run-script install:with-sqlite $install_path --timeout=0 --working-dir $install_path
+
+    # Start the built-in PHP server.
+	composer run-script start-contenta $install_path --timeout=0 --working-dir $install_path
 	cd $original_wd
 else
     echo -e "\nBye!"
     exit
 fi
-rm -Rf $mytmpdir
