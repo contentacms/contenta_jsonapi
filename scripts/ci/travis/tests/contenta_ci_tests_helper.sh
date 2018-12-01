656d2427944dad44f3f350b8a12c45308d4fcbf0
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2018-12-01 16:57:29 +0100

    test: change testing strategy

--- scripts/ci/travis/tests/contenta_ci_tests_helper.sh
+++ scripts/ci/travis/tests/contenta_ci_tests_helper.sh
@@ -3,29 +3,6 @@
 COMPOSER_BIN_DIR="$(composer config bin-dir)"
 DOCROOT="web"
 
-# Setup Anonymous User Function
-# Gives access to anonymous user to access protected resources.
-# This function receives one argument:
-#   $1 -> The Drupal Base Path
-setup_anonymous_user() {
-     if [ -z $1 ] ; then
-        echo "Please pass the Contenta Project Base Path to the install_test_dependencies function " 1>&2
-        exit 1
-    fi
-
-    # Setup local variables
-    current_path=`pwd`
-    DRUSH=$1/$COMPOSER_BIN_DIR/drush
-    DRUPAL_BASE=$1/$DOCROOT
-
-    cd $DRUPAL_BASE
-    # Add Permission to anonymous user
-    $DRUSH updatedb -y
-    $DRUSH cr -y
-
-    cd $current_path
-}
-
 # Run Functional Tests Function
 # Run Contenta CMS Functional Tests
 # This function receives one argument:
@@ -36,18 +13,25 @@ run_functional_tests() {
         exit 1
     fi
 
-    if [[ -z $WEB_HOST ]] || [[ -z $WEB_PORT ]] ; then
-        echo "Please ensure that WEB_HOST and WEB_PORT environment variables are set." 1>&2
+    if [[ -z $SIMPLETEST_BASE_URL ]] ; then
+        echo "Please ensure that SIMPLETEST_BASE_URL environment variable is set. Ex: http://localhost" 1>&2
+        exit 1
+    fi
+    if [[ -z $SIMPLETEST_DB ]] ; then
+        echo "Please ensure that SIMPLETEST_DB environment variable is set. Ex: mysql://username:password@localhost/databasename#table_prefix" 1>&2
         exit 1
     fi
 
-    current_path=`pwd`
+    # remove xdebug to make php execute faster
+    phpenv config-rm xdebug.ini
+
     CONTENTA_PATH=$1/$DOCROOT/profiles/contrib/contenta_jsonapi/
-    PHPUNIT=$1/$COMPOSER_BIN_DIR/phpunit
-
+    DRUSH=$1/$COMPOSER_BIN_DIR/drush
+    cd $1/$DOCROOT
+    $DRUSH pm-enable --yes simpletest
     cd $CONTENTA_PATH
-
-    WEB_HOST=$WEB_HOST WEB_PORT=$WEB_PORT $PHPUNIT --testsuite ContentaFunctional
+    echo "php $1/$DOCROOT/core/scripts/run-tests.sh --php `which php` --url $SIMPLETEST_BASE_URL --suppress-deprecations --verbose --color Contenta"
+    php $1/$DOCROOT/core/scripts/run-tests.sh --php `which php` --url $SIMPLETEST_BASE_URL --suppress-deprecations --verbose --color Contenta
     exit $?
 }
 
--- tests/src/Functional/InstallationTest.php
+++ tests/src/Functional/InstallationTest.php
@@ -3,15 +3,18 @@
 namespace Drupal\Tests\contenta_jsonapi\Functional;
 
 use Drupal\Component\Serialization\Json;
-use PHPUnit\Framework\TestCase;
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
 use GuzzleHttp\Client;
 
 /**
  * Tests that installation finished correctly and known resources are available.
  *
- * @group ContentaInstaller
+ * @group Contenta
  */
-class InstallationTest extends TestCase {
+class InstallationTest extends BrowserTestBase {
+
+  public $profile = 'contenta_jsonapi';
 
   /**
    * @var Client
@@ -19,29 +22,25 @@ class InstallationTest extends TestCase {
   private $httpClient;
 
   /**
-   * @var string
-   */
-  private $baseUrl;
-
-  /**
    * {@inheritdoc}
    */
   protected function setUp() {
-    $url = getenv('WEB_HOST');
-    $port = getenv('WEB_PORT');
-    $this->baseUrl = "http://$url:$port";
+    parent::setUp();
 
     // Set up a HTTP client that accepts relative URLs.
     $this->httpClient = new Client(['http_errors' => FALSE]);
   }
 
   public function testLandingPage() {
-    $response = $this->httpClient->request('GET', $this->baseUrl . '/');
-    $this->assertEquals(200, $response->getStatusCode());
+    $url = Url::fromRoute('<front>', [], ['absolute' => TRUE])->toString();
+    $this->getSession()->visit($url);
+    $this->assertEquals(200, $this->getSession()->getStatusCode());
   }
 
   public function testKnownResources() {
-    $response = $this->httpClient->request('GET', $this->baseUrl . '/api');
+    $url = Url::fromRoute('jsonapi.resource_list', [], ['absolute' => TRUE])
+      ->toString();
+    $response = $this->httpClient->request('GET', $url);
     $body = $response->getBody()->getContents();
     $output = Json::decode($body);
     $resources = array_keys($output['links']);
@@ -69,9 +68,10 @@ class InstallationTest extends TestCase {
   }
 
   public function testRpcMethod() {
+    $url = Url::fromRoute('jsonrpc.handler', [], ['absolute' => TRUE])->toString();
     $response = $this->httpClient->request(
       'GET',
-      $this->baseUrl . '/jsonrpc',
+      $url,
       [
         'query' => [
           'query' => '{"jsonrpc":"2.0","method":"jsonapi.metadata","id":"cms-meta"}'
@@ -83,7 +83,7 @@ class InstallationTest extends TestCase {
     $this->assertEquals('/api', $output['result']['openApi']['basePath']);
     $response = $this->httpClient->request(
       'POST',
-      $this->baseUrl . '/jsonrpc',
+      $url,
       ['body' => '{"jsonrpc":"2.0","method":"jsonapi.metadata","id":"cms-meta"}']
     );
     $body = $response->getBody()->getContents();
@@ -93,27 +93,49 @@ class InstallationTest extends TestCase {
   }
 
   public function testJsonApiEntryPoint() {
+    $url = Url::fromRoute('jsonapi.resource_list', [], ['absolute' => TRUE])
+      ->toString();
     $response = $this->httpClient->request(
       'GET',
-      $this->baseUrl . '/api',
-      [
-        'query' => [
-          'query' => '{"jsonrpc":"2.0","method":"jsonapi.metadata","id":"cms-meta"}'
-        ],
-        'headers' => ['Accept' => 'application/vnd.api+json'],
-      ]);
+      $url,
+      ['headers' => ['Accept' => 'application/vnd.api+json']]
+    );
     $this->assertSame(200, $response->getStatusCode());
     $body = $response->getBody()->getContents();
     $output = Json::decode($body);
-    $this->assertArrayHasKey('self', $output);
-    $this->assertArrayHasKey('node--recipe', $output);
+    $links = $output['links'];
+    $this->assertArrayHasKey('href', $links['self']);
+    $this->assertArrayHasKey('href', $links['recipes']);
   }
 
   public function testOpenApi() {
-    $this->getSession()->visit('/admin/api');
-    $this->assertNotEmpty(
-      $this->getSession()->getPage()->find('css', 'a[href="#tag/Content-Recipe"]')
+    $url = Url::fromRoute('contenta_enhancements.api', [], ['absolute' => TRUE])->toString();
+    $this->getSession()->visit($url);
+    $page = $this->getSession()->getPage();
+    $open_api_spec = Json::decode(
+      html_entity_decode($page->find('css', '#redoc-ui')->getAttribute('spec'))
     );
+    $definitions = [
+      'comment--comment',
+      'file--file',
+      'media--image',
+      'node--article',
+      'node--embeddable',
+      'node--page',
+      'node--recipe',
+      'node--tutorial',
+      'taxonomy_term--category',
+      'taxonomy_term--ingredients',
+      'taxonomy_term--recipe_category',
+      'taxonomy_term--recipe_cuisine',
+      'taxonomy_term--tags',
+      'user--user',
+      'menu_link_content--menu_link_content',
+    ];
+    array_walk($definitions, function ($definition) use ($open_api_spec) {
+      $this->assertArrayHasKey($definition, $open_api_spec['definitions']);
+    });
+    $this->assertEquals('OpenAPI Documentation', $page->find('css', 'h1.page-title')->getText());
   }
 
 }
